3ae3fc6167ace8c03e4274b6e0d8182bc30be783
Xfervent123 <123369106+Xfervent123@users.noreply.github.com>
2025-12-04 21:09:13 +0300

    Create Задание _Сокращенный синтаксис_.cs

---
+++ Основы ООП/Задание _Сокращенный синтаксис_.cs
@@ -0,0 +1,27 @@
+/*
+╔════════════════════════════════════════════════════════════════════╗
+║                       Сокращенный синтаксис                        ║
+╚════════════════════════════════════════════════════════════════════╝
+
+Ваша команда пишет программу с оконным интерфейсом, и вам надо реализовать инициализацию меню.
+Для каждого пункта меню указывается название, горячая клавиша (далее указана в скобках) и список подменю (null, если подменю нет).
+На верхнем уровне должно находиться два пункта: File (F) и Edit (E).
+Меню File должно содержать команды New (N), Save (S).
+Меню Edit (E) должно содержать команды Copy (C) и Paste (V).
+Решите задачу в одно выражение с использованием сокращенного синтаксиса создания объектов. Используйте переводы строк и отступы, чтобы сделать код более читаемым.
+
+public class MenuItem
+{
+    public string Caption;
+    public string HotKey;
+    public MenuItem[] Items;
+}
+*/
+
+public static MenuItem[] GenerateMenu()
+{
+    return new[] {
+		new MenuItem { Caption = "File", HotKey = "F", Items = new[] { new MenuItem { Caption = "New", HotKey = "N"}, new MenuItem { Caption = "Save", HotKey = "S"} }},
+		new MenuItem { Caption = "Edit", HotKey = "E", Items = new[] {new MenuItem { Caption = "Copy", HotKey = "C"}, new MenuItem { Caption = "Paste", HotKey = "V"} }}
+	};
+}
